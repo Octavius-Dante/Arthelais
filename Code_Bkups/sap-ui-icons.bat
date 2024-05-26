2d91ec082e11462c7eac3221294066a82004dc8e
Lordes Dante Epicurus <vivi.lordes@gmail.com>
2024-05-26 01:17:39 -0700

    ss

---
+++ Code_Bkups/sap-ui-icons.bat
@@ -0,0 +1,6 @@
+
+start chrome "https://ui5.sap.com/test-resources/sap/m/demokit/iconExplorer/webapp/index.html"
+
+start chrome "https://sapui5.hana.ondemand.com/sdk/test-resources/sap/m/demokit/iconExplorer/webapp/index.html#"
+
+start chrome "https://sdk.openui5.org/1.38.62/iconExplorer.html"
